16cd21f7f024fc476e3249266407188ca1137241
sooyoung <salutluvya@gmail.com>
2023-05-20 10:11:41 +0200

    Add delete and update api

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.locals.data = [
     {
+        id: 0,
         deviceName: 'Samsung G',
         ownerName: 'Tom',
         deviceType: 'Tablet',
@@ -15,24 +16,40 @@ app.locals.data = [
     }
 ];
 
-app.get("/api", (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.get("/list", (req, res) => {
+app.get('/list', (req, res) => {
     res.json(app.locals.data);
 });
 
-app.post("/update", (req, res) => {
-    res.set({
-        "Content-Type": "application/json",
-    });
-
+app.post('/add', (req, res) => {
     app.locals.data = [...app.locals.data, req.body];
     res.json(app.locals.data);
+});
+
+app.post('/update', (req, res) => {
+    const data = app.locals.data;
+    const index = data.findIndex(item => item.id === req.body.id);
+
+    if(index < 0) {
+        res.status(400).json({ message: 'There is no item'})
+    } else {
+        data.splice(index, 1, req.body);
+        app.locals.data = [...data];
+    }
+    res.json(app.locals.data);
+})
+
+app.delete('/:id', (req, res) => {
+    const data = app.locals.data;
+    const newData = data.filter(item => item.id !== parseInt(req.params.id));
+        app.locals.data = [...newData];
+
+        res.json(app.locals.data);
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
